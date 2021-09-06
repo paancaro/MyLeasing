21168481c8ab2de50d2f3853e0435b863eccc593
ryukendenz <castrop@gmail.com>
2021-09-05 20:03:44 -0500

    Complete database

---
+++ MyLeasing.Web/Data/Entities/Contract.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Contract
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Remarks { get; set; }
+
+        [Display(Name = "Price")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Start Date")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Is Active?")]
+        public bool IsActive { get; set; }
+
+        private Property property;
+
+        public Property GetProperty()
+        {
+            return property;
+        }
+
+        public void SetProperty(Property value)
+        {
+            property = value;
+        }
+
+        private Owner owner;
+
+        public Owner GetOwner()
+        {
+            return owner;
+        }
+
+        public void SetOwner(Owner value)
+        {
+            owner = value;
+        }
+
+        private Lessee lessee;
+
+        public Lessee GetLessee()
+        {
+            return lessee;
+        }
+
+        public void SetLessee(Lessee value)
+        {
+            lessee = value;
+        }
+
+        [Display(Name = "Start Date")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime StartDateLocal => StartDate.ToLocalTime();
+
+        [Display(Name = "End Date")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime EndDateLocal => EndDate.ToLocalTime();
+
+        public Property Property { get; set; }
+        public Lessee Lessee { get; set; }
+        public Owner Owner { get; set; }
+
+    }
+}
---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30, ErrorMessage = "El campo {0} no debe tener mas de {1} caracteres.")]
+        [Display(Name = "Documento")]
+        public string Document { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Primer Nombre")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Apellido")]
+        public string LastName { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Telefono Fijo")]
+        public string FixedPhone { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Telefono Celular")]
+        public string CellPhone { get; set; }
+
+        [MaxLength(200)]
+        [Display(Name = "Direccion")]
+        public string Address { get; set; }
+        
+        [Display(Name = "Nombre Completo")]
+   
+        public string FullName => $"{FirstName} {LastName}";
+        [Display(Name = "Nombre Completo")]
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+        public ICollection<Property> Properties { get; set; }
+
+        public ICollection<Contract> Contracts { get; set; }
+
+
+    }
+}
